75722308e33df29cc2fe9430ae82ba846cb0a586
Edwin A. García Maradiaga <egarcia@unicah.edu>
2021-02-22 18:55:53 -0600

    CRUD Node-MySQL completo

--- routes/api2/vehiculos.js
+++ routes/api2/vehiculos.js
@@ -12,9 +12,74 @@ var con = mysql.createPool({
     multipleStatements:true
 });
 
+//Obtener el listado completo de vehiculos
 router.get('/get_vehiculos', (req, res, next) => {
     var query = 'select * from vehiculos';
     con.query(query, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+//Obtener un vehiculos por placa
+router.get('/get_vehiculo', (req, res, next) => {
+    var query = 'select * from vehiculos where placa = ?';
+    var values = [req.query.placa];
+
+    con.query(query, values, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+//Insertar un nuevo vehiculo
+router.post('/insert_vehiculo', (req, res, next) => {
+    var query = 'INSERT INTO Vehiculos (placa, color, marca, modelo) values (?, ?, ?, ?)';
+    var values = [req.body.placa,
+                  req.body.color,
+                  req.body.marca,
+                  req.body.modelo];
+
+    con.query(query, values, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+//Modificar la informacion de un vehiculo
+router.put('/update_vehiculo', (req, res, next) => {
+    var query = 'Update Vehiculos set color=?, marca=?, modelo=? WHERE placa = ?';
+    
+    var values = [req.body.color,
+                  req.body.marca,
+                  req.body.modelo,
+                  req.body.placa];
+
+    con.query(query, values, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+//Borrar un vehiculo
+router.delete('/delete_vehiculo', (req, res, next) => {
+    var query = 'delete from vehiculos.vehiculos where placa = ?';
+    
+    var values = [req.query.placa];
+
+    con.query(query, values, (err, result, field) => {
        if(err){
            next(err);
        } else {
